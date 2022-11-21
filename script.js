4302fc506494a5fd31edc3b529230888da3b8b27
Barbara Viana <81521722+BarbaraViana@users.noreply.github.com>
2022-11-21 17:33:44 -0300

    Update script.js
    *

--- script.js
+++ script.js
@@ -5,15 +5,33 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        res.innerHTML = `Não foi possivel contar`
         alert('[ERRO] faltam dados')
+
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando...<br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
+        if (p <= 0) {
+            window.alert('Passo Inválido, considerando passo 1')
+            p = 1
         }
+
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+
+        } else {
+            //contagem regressiva 
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}` // o codigo do icon só funciona entre crase
+
+
     }
 }
